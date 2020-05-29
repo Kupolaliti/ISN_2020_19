1d938c7861dab4417ab46ec6ef520df93b7d4c1f
Kupolaliti <55890418+Kupolaliti@users.noreply.github.com>
2020-05-29 20:48:09 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+	
+<figure>
+
+   <MyPicture/>
+   <MyText msg="За кис-кис отвечаю!" />
+   
+</figure>
+
+    <MyButton msg="Хорошо"/>
+
+  </div>
+</template>
+
+<script>
+
+import MyButton from './components/MyButton.vue'
+import MyPicture from './components/MyPicture.vue'
+import MyText from './components/MyText.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyButton,
+	MyPicture,
+	MyText
+  }
+}
+
+</script>
+
+<style>
+   figure {
+    width: 280px; 
+	height: 400px;
+    float: center; 
+    margin: 5%; 
+	margin-top: 50px;
+    background: #ffc107; 
+    border-radius: 5px; 
+    padding: 2%; 
+	text-align: center;
+   }
+
+   </style>
+
