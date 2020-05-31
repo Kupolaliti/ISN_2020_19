22feabdcca4019146958ab71793cfe696b8c49a4
Kupolaliti <55890418+Kupolaliti@users.noreply.github.com>
2020-05-31 16:54:25 +0300

    Add files via upload

---
+++ src/components/MyText.vue
@@ -0,0 +1,28 @@
+<template>
+
+	 <p>{{msg}}</p>
+	
+</template>
+
+<script>
+export default {
+  name: 'MyText',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+p{
+font-family: Suranna;
+font-style: normal;
+font-weight: normal;
+font-size: 24px;
+line-height: 53px;
+text-align: center;
+letter-spacing: 0.04em;
+color: #FFFAFA;
+}
+</style>
+}
---
+++ src/components/MyTextBox.vue
@@ -0,0 +1,15 @@
+<template>
+	<b-form-input class="input" placeholder="+7 ... ......." id="MyTextBox">{{name}}</b-form-input>
+</template>
+
+<script>
+export default {
+  name: 'MyTextBox',
+  props: {
+    
+  }
+}
+
+</script>
+
+}
